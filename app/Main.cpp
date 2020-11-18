32366fb8acc4c316f8bcb00c999e831433b32f46
Cedric <cedric.attavay@surgiqual-institute.com>
2020-11-18 13:35:50 +0100

    2nd step: Create our 1st CMake target (executable)

---
+++ app/Main.cpp
@@ -0,0 +1,12 @@
+//! \file Main.cpp
+//! \brief Implements CMain
+//! \author http://www.surgiqual-institute.com/
+//! \date November 2020
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
